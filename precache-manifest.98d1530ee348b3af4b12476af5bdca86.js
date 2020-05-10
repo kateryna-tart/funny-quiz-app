711a9acd7b5977b1f4fef3203dbe5a7f0c74b4ee
Kateryna Moisieieva <katerynamoisieieva@katerynas-air.home>
2020-05-10 14:36:07 -0400

    Updates

---
+++ precache-manifest.98d1530ee348b3af4b12476af5bdca86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8be9c8afceecfadce802edcf6ffecf4",
+    "url": "/funny-quiz-app/index.html"
+  },
+  {
+    "revision": "ca602ea73044d9e74555",
+    "url": "/funny-quiz-app/static/css/main.f0ac30fe.chunk.css"
+  },
+  {
+    "revision": "91cd5a9bce6277011ba4",
+    "url": "/funny-quiz-app/static/js/2.1d477258.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funny-quiz-app/static/js/2.1d477258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca602ea73044d9e74555",
+    "url": "/funny-quiz-app/static/js/main.f572d708.chunk.js"
+  },
+  {
+    "revision": "00331c2742ab490e723d",
+    "url": "/funny-quiz-app/static/js/runtime-main.47439051.js"
+  }
+]);
